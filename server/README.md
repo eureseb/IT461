05aadd2ff557fc1251da748239f200ae567670dc
Eurese Antonio Bustamante <eurese.bustamante@orangeandbronze.com>
2022-05-05 14:02:29 +0800

    Added exercise 2 files

--- server/README.md
+++ server/README.md
@@ -1,19 +1,10 @@
 # Installation instructions
-
 ## Build the containers
-
 ### $ docker-compose build
-
 ## Start the containers
-
 ### $ docker-compose up
-
 ## Shutdown the containers
-
 ### $ docker-compose down
 
 # Usage
-
 ## Use Postman to send request to http://127.0.0.1:6000
-
-# Hello world
--- server/src/app.py
+++ server/src/app.py
@@ -1,109 +1,11 @@
-from flask import Flask, jsonify, request, make_response, g
-from models.dog import Dog
-from models.cat import Cat
-from db import Db
-
+from flask import Flask, Blueprint
+from v1.dog.router import DogRouter
+from v1.cat.router import CatRouter
 app = Flask(__name__)
 
-#g.db = Db()
-#g.db.connect()
-
-@app.route('/')
-def home():
-    # Experimental root
-    links = { "_links": {"citu-api:dogs": "http://127.0.0.1:6000/dogs", "cit-api:cats": "http://127.0.0.1:6000/cats"} }
-    return jsonify(links)
-
-@app.route('/dogs', methods=['POST', 'GET', 'PUT', 'DELETE'])
-def dogs():
-    dog = Dog()
-    if str(request.method).upper() == 'POST':
-        resp = dog.post(request.json)
-        if resp == False:
-            return make_response(jsonify({
-                "error": "Failed to add. There are items in your request that are invalid."
-            }), 400)
-        return jsonify(resp)
-    if str(request.method).upper() == 'PUT':
-        return jsonify(dog.put(request.json))
-    if str(request.method).upper() == 'DELETE':
-        return jsonify(dog.delete(request.json))
-    dogs = dog.get()
-    
-    
-    dogs_returns = {}
-    dogs_returns["dogs"] = dogs
-    dogs_returns["_links"] = { "root": "http://127.0.0.1:6000/", "self": "http://127.0.0.1:6000/dogs/"} 
-    return jsonify(dogs_returns)
-
-@app.route('/dogs/<dog_id>', methods=['GET', 'PUT', 'DELETE'])
-def dog(dog_id):
-    dog_object = Dog()
-    dog = dog_object.get({"id": dog_id})
-    if dog is None:
-        return make_response(jsonify({"error": "Dog id not found."}), 404)
-    if str(request.method).upper() == 'PUT':
-        dog_data = request.json
-        dog_data['id'] = dog_id
-        resp = dog_object.put(dog_data)
-        if resp == False:
-            return make_response(jsonify({
-                "error": "Failed to update. There are items in your request that are invalid."
-            }), 400)
-        return jsonify(resp)
-    if str(request.method).upper() == 'DELETE':
-        if(dog_object.delete(dog_id) == 1):
-            return jsonify({ "cit-api:dogs": "http://127.0.0.1:6000/dogs"})
-    
-    dog_returns = {}
-    dog_returns["dog"] = dog
-    dog_returns["_links"] = { "cit-api:dogs": "http://127.0.0.1:6000/dogs", "self": "http://127.0.0.1:6000/dogs/"+dog_id} 
-    return jsonify(dog_returns)
-
-@app.route('/cats', methods=['POST', 'GET', 'PUT', 'DELETE'])
-def cats():
-    cat = Cat()
-    if str(request.method).upper() == 'POST':
-        resp = cat.post(request.json)
-        if resp == False:
-            return make_response(jsonify({
-                "error": "Failed to add. There are items in your request that are invalid."
-            }), 400)
-        return jsonify(resp)
-    if str(request.method).upper() == 'PUT':
-        return jsonify(cat.put(request.json))
-    if str(request.method).upper() == 'DELETE':
-        return jsonify(cat.delete(request.json))
-    cats = cat.get()
-
-    cats_returns = {}
-    cats_returns["cats"] = cats
-    cats_returns["_links"] = { "root": "http://127.0.0.1:6000/", "self": "http://127.0.0.1:6000/cats/"} 
-    return jsonify(cats_returns)
-
-@app.route('/cats/<cat_id>', methods=['GET', 'PUT', 'DELETE'])
-def cat(cat_id):
-    cat_object = Cat()
-    cat = cat_object.get({"id": cat_id})
-    if cat is None:
-        return make_response(jsonify({"error": "cat id not found."}), 404)
-    if str(request.method).upper() == 'PUT':
-        cat_data = request.json
-        cat_data['id'] = cat_id
-        resp = cat_object.put(cat_data)
-        if resp == False:
-            return make_response(jsonify({
-                "error": "Failed to update. There are items in your request that are invalid."
-            }), 400)
-        return jsonify(resp)
-    if str(request.method).upper() == 'DELETE':
-        if(cat_object.delete(cat_id) == 1):
-            return jsonify({ "cit-api:cats": "http://127.0.0.1:6000/cats"})
-    
-    cat_returns = {}
-    cat_returns["cat"] = cat
-    cat_returns["_links"] = { "cit-api:cats": "http://127.0.0.1:6000/cats", "self": "http://127.0.0.1:6000/cats/"+cat_id} 
-    return jsonify(cat_returns)
+bp_dogs = Blueprint('dogs', __name__, url_prefix='/v1/dogs')
+DogRouter.handler(bp_dogs)
+app.register_blueprint(bp_dogs)
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=6000)
