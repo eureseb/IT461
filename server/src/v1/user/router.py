16b635cab5d1a1dc710838f7f255e427626fb536
Eurese Antonio Bustamante <eurese.bustamante@orangeandbronze.com>
2022-06-01 16:17:07 +0800

    fixed bug

--- server/src/v1/user/router.py
+++ server/src/v1/user/router.py
@@ -1,4 +1,3 @@
-
 from flask import Blueprint
 from v1.user.controller import UserController
 from v1.auth import jwt_token_required
@@ -16,4 +15,4 @@ class UserRouter():
         app.add_url_rule('/<user_id>', methods=['GET'], view_func=controller.get)
         app.add_url_rule('/<user_id>', methods=['PUT'], view_func=controller.put)
         app.add_url_rule('/<user_id>', methods=['DELETE'], view_func=controller.delete)
-        return 
+        return app
